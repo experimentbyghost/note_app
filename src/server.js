3191e1f3f2d16c60036c9619d848aec792c7b516
TejasAghade <tejasaghade1@gmail.com>
2023-01-11 11:40:06 +0530

    minor

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-
+const PORT = process.env.PORT || 5000;
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended:false}))
@@ -22,7 +22,7 @@ mongoose.connect("mongodb+srv://notes_app123:cPzqS086oitbLiGN@cluster0.zpvfxfw.m
 
 })
 
-const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, function(){
     console.log("server started on port", PORT);
 })

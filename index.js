02de2a5567cb271bb3644f536f21873ec1267000
experimentbyghost <122426920+experimentbyghost@users.noreply.github.com>
2023-01-12 21:02:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended:false}))
+// true -> can send nested object
+// false -> can't send nested object
+
+app.use(bodyParser.json());
+
+
+// mongodb configuration
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+mongoose.connect("mongodb+srv://notes_app123:cPzqS086oitbLiGN@cluster0.zpvfxfw.mongodb.net/?retryWrites=true&w=majority").then(function(){
+
+    const noteRouter = require("./src/routes/noteRoutes");
+    app.use("/notes", noteRouter);
+
+})
+
+
+app.listen(PORT, function(){
+    console.log("server started on port", PORT);
+})
